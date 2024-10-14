acdb9a9709c8efde00011ff02a3bfc0dea6caf7e
Avishka Chasith Gunathilaka <chasithgunathilaka@gmail.com>
2024-10-15 00:51:53 +0530

    second update

---
+++ AngularProject/src/app/components/roles/roles.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles',
+  standalone: true,
+  imports: [],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css'
+})
+export class RolesComponent {
+  // DataTypes - string, number, boolean, Date, Object, null, undefined
+  firstName:string="Angular Framework";
+  AngularVersion ="version 17";
+  currentVersion:number=18;
+  isActive:boolean=false;
+  currentDate:Date=new Date();
+
+
+}
